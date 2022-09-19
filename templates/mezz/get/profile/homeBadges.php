a1d69679d97b4520f34b8bfd82b3d4e98c572a0c
Lucifer <nadiecaca2000@gmail.com>
2022-09-19 17:22:30 +0200

    refactactor

--- templates/mezz/get/profile/homeBadges.php
+++ templates/mezz/get/profile/homeBadges.php
@@ -2,21 +2,19 @@
     <h2 class="page-content-collider-content-profile-card-wrapper-aligner-content-title">Placas</h2>
 
     <?php
-        $userId = userHome('id');
-        $stmt = $dbh->prepare("SELECT * FROM user_badges WHERE user_id = :userid LIMIT 14");
-        $stmt->bindParam(':userid', $userId);
-        $stmt->execute();
-            if (!$stmt->RowCount() == 0) {
-                while($badge = $stmt->fetch()) {
+    $userId = userHome('id');
+    $stmt = $dbh->prepare("SELECT * FROM user_badges WHERE user_id = :userid LIMIT 14");
+    $stmt->bindParam(':userid', $userId);
+    $stmt->execute();
+    if (!$stmt->RowCount() == 0) {
+        while ($badge = $stmt->fetch()) {
     ?>
-        <img src=<?=$config['badgeURL']?><?=filter($badge["badge_id"])?>.gif class='page-content-collider-content-profile-card-wrapper-aligner-content-badge' data-toggle='tooltip' data-original-title='<?=filter($badge["badge_id"])?>' style='padding: 5px 5px;
+            <img src=<?= $config['badgeURL'] ?><?= filter($badge["badge_id"]) ?>.gif class='page-content-collider-content-profile-card-wrapper-aligner-content-badge' data-toggle='tooltip' data-original-title='<?= filter($badge["badge_id"]) ?>' style='padding: 5px 5px;
         margin: 0 20px 20px 0;'>
     <?php
         }
-        }
-        else
-        {
-            echo userHome('username').' no hay placas en el momento!';
-        }
+    } else {
+        echo userHome('username') . ' no hay placas en el momento!';
+    }
     ?>
 </div>
--- templates/mezz/get/profile/homePhotos.php
+++ templates/mezz/get/profile/homePhotos.php
@@ -2,27 +2,23 @@
     <h2 class="page-content-collider-content-profile-photos-title">Fotos</h2>
 
     <?php
-        $userId = userHome('id');
-        $stmt = $dbh->prepare("SELECT * FROM user_photos WHERE user_id = :userid LIMIT 4");
-        $stmt->bindParam(':userid', $userId);
-        $stmt->execute();
-        if (!$stmt->RowCount() == 0)
-        {
-            while($photos = $stmt->fetch())
-            {
+    $userId = userHome('id');
+    $stmt = $dbh->prepare("SELECT * FROM user_photos WHERE user_id = :userid LIMIT 4");
+    $stmt->bindParam(':userid', $userId);
+    $stmt->execute();
+    if (!$stmt->RowCount() == 0) {
+        while ($photos = $stmt->fetch()) {
     ?>
-        <div class='page-content-collider-content-profile-photo'>
+            <div class='page-content-collider-content-profile-photo'>
 
-            <span class='page-content-collider-content-profile-photo-promo pixelated' style='background-image: url(<?php echo $config['roomphotos'] ?><?=filter($photos["photo"])?>.png)'></span>
-        
-        </div>
+                <span class='page-content-collider-content-profile-photo-promo pixelated' style='background-image: url(<?php echo $config['roomphotos'] ?><?= filter($photos["photo"]) ?>.png)'></span>
+
+            </div>
     <?php
-            }
         }
-        else
-        {
-            echo userHome('username').' no tiene fotos en este momento.';
-        }
+    } else {
+        echo userHome('username') . ' no tiene fotos en este momento.';
+    }
     ?>
-    
+
 </div>
